c7406bc91a6e3fc98031191ed77f8c64d2ef3f80
sohamaggarwal8623-web <sohamaggarwal8623@gmail.com>
2026-07-14 16:03:30 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
 
   return (
-     <div className='bg-[url("D:\FRONTEND\React\password\public\bg.photo.png")]
+     <div className='bg-[url("/bg.photo.png")]
      h-screen bg-cover flex flex-col items-center gap-4'>
 
       <h1 className='text-amber-50 text-center text-5xl'>Password Generator</h1>
